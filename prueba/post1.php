42542b3ee44398d3c6dfe96a0ca78eef0fc46687
CRIISTHI4N <86174931+CRIISTHI4N@users.noreply.github.com>
2023-01-13 13:56:32 -0500

    Add files via upload
    web services

---
+++ prueba/post1.php
@@ -0,0 +1,89 @@
+<?php
+include "config.php";
+include "utils.php";
+
+$dbConn =  connect($db);
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET')
+{
+    if (isset($_GET['cedulaPaciente']))
+    {
+      //Mostrar un post
+      $sql = $dbConn->prepare("SELECT * paciente  where cedulaPaciente=:cedulaPaciente");
+      $sql->bindValue(':cedulaPaciente', $_GET['cedulaPaciente']);
+      $sql->execute();
+      header("HTTP/1.1 200 OK");
+      echo json_encode(  $sql->fetch(PDO::FETCH_ASSOC)  );
+      exit();
+    }
+    else {
+      //Mostrar lista de post
+      $sql = $dbConn->prepare("SELECT * FROM paciente");
+      $sql->execute();
+      $sql->setFetchMode(PDO::FETCH_ASSOC);
+      header("HTTP/1.1 200 OK");
+      echo json_encode( $sql->fetchAll()  );
+      exit();
+    }
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $input = $_POST;
+    $sql = "INSERT INTO citamedica
+    (idCita, idPaciente, nombreMedico, especialidad, fechaCita, horaCita)
+    VALUES
+    (:idCita, :idPaciente, :nombreMedico, :especialidad, :fechaCita, :horaCita)";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+    $statement->execute();
+
+ 
+
+    $postCodigo = $dbConn->lastInsertId();
+    if($postCodigo)
+    {
+      $input['cedulaPaciente'] = $postCodigo;
+      header("HTTP/1.1 200 OK");
+      echo json_encode($input);
+      exit();
+     }
+}
+
+ 
+
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE')
+{
+    $codigo = $_GET['cedulaPaciente'];
+  $statement = $dbConn->prepare("DELETE FROM paciente where cedulaPaciente=:cedulaPaciente");
+  $statement->bindValue(':cedulaPaciente', $codigo);
+  $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+
+ 
+
+if ($_SERVER['REQUEST_METHOD'] == 'PUT')
+{
+    $input = $_GET;
+    $postCodigo = $input['cedulaPaciente'];
+    $fields = getParams($input);
+
+ 
+
+    $sql = "
+          UPDATE paciente
+          SET $fields
+          WHERE cedulaPaciente='$postCodigo'
+           ";
+    $statement = $dbConn->prepare($sql);
+    bindAllValues($statement, $input);
+
+ 
+
+    $statement->execute();
+    header("HTTP/1.1 200 OK");
+    exit();
+}
+?>
