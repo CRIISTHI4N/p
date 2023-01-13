958fe19a5f53b99e5ae9b626fdd0e665d5a1f46d
CRIISTHI4N <gamers1235velez@gmail.com>
2023-01-13 13:39:10 -0500

    ni

---
+++ EstudiantesITQ/ListadoEstudiantes.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EstudiantesITQ
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListadoEstudiantes : ContentPage
+
+
+    {
+
+        private const string Url = "http://10.20.23.200/itq/post.php";
+
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Datos> _post;
+        public int codigo = -1;
+        public string nombre, apellido, correo, telefono;
+
+        public ListadoEstudiantes()
+        {
+            InitializeComponent();
+            Get();
+
+        }
+
+        public async void Get()
+        {
+            try
+            {
+                var content = await client.GetStringAsync(Url);
+                List<Datos> post = JsonConvert.DeserializeObject<List<Datos>>(content);
+                _post = new ObservableCollection<Datos>(post);
+
+                lstViewEstudiantes.ItemsSource = post;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
